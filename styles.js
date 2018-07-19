55ed71634f74613f4db35569d9311e6b372d1bb9
Ablizzy33 <41392903+Ablizzy33@users.noreply.github.com>
2018-07-19 06:48:25 -0700

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,29 @@
+function popup(){
+alert("WASUPPPP TO MY VIEWERS!");
+}
+
+function changeImage() {
+  var image = document.getElementById('eleven');
+if (image.src.match("http://78.media.tumblr.com/257f2e3dea37b418e08247f8971db32a/tumblr_mmtb9lOFvu1s7vbhqo1_1280.gif")) {
+image.src = "https://media0.giphy.com/media/EAHtlUWzVvFM4/giphy.gif";
+     }
+else {
+image.src= "http://78.media.tumblr.com/257f2e3dea37b418e08247f8971db32a/tumblr_mmtb9lOFvu1s7vbhqo1_1280.gif";
+     }
+                       }
+
+function changeImage2(){
+  var image = document.getElementById('check');
+  if
+  (image.src.match("https://78.media.tumblr.com/e3fa26a4827c0fde0769a8a8185a9285/tumblr_on81sbSPUX1qm9s2io1_500.gif"))
+  {
+    image.src = "http://gifimage.net/wp-content/uploads/2017/10/nike-gif-5.gif";
+  }
+else {
+image.src= "https://78.media.tumblr.com/e3fa26a4827c0fde0769a8a8185a9285/tumblr_on81sbSPUX1qm9s2io1_500.gif";
+}
+
+
+
+
+}
